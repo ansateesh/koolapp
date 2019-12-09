afeeb34fdfc417473b3b4887ca472bef2a756722
ansateesh <ansateesh@yahoo.com>
2019-12-09 10:10:17 +0530

    adding index

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -20,6 +20,17 @@ router.get('/products', function (req, res) {
                 items : []
             }
             var item = {};
+            
+            var indexQuery = "CREATE FULLTEXT INDEX XXIBM_PRODUCT_SKU_FTS_INDEX ON XXIBM_PRODUCT_SKU(DESCRIPTION,LONG_DESCRIPTION, SKU_ATTRIBUTE_VALUE1, SKU_ATTRIBUTE_VALUE2)"
+            
+            connection.query(indexQuery, function(err, res, fields){
+                if(err){
+                    console.log("Error creating index")
+                    console.log(err);
+                }else{
+                    console.log("Created Index");
+                }
+            });
           
             //var query = 'select a.item_number, a.description, a.long_description, a.catalogue_category, a.sku_unit_of_measure, a.sku_attribute1, a.sku_attribute_value1, a.sku_attribute2, a.sku_attribute_value2, a.sku_attribute3, a.sku_attribute_value3, b.list_price, b.discount, b.in_stock, c.brand from xxibm_product_sku a, xxibm_product_pricing b, xxibm_product_style c where a.item_number=b.item_number and c.item_number=round(a.item_number/1000) * 1000';
             var query = 'select a.ITEM_NUMBER, a.DESCRIPTION, a.LONG_DESCRIPTION, a.CATALOGUE_CATEGORY, a.SKU_UNIT_OF_MEASURE, a.SKU_ATTRIBUTE1, a.SKU_ATTRIBUTE_VALUE1, a.SKU_ATTRIBUTE2, a.SKU_ATTRIBUTE_VALUE2, a.SKU_ATTRIBUTE3, a.SKU_ATTRIBUTE_VALUE3, b.LIST_PRICE, b.DISCOUNT, b.IN_STOCK, c.BRAND from XXIBM_PRODUCT_SKU a, XXIBM_PRODUCT_PRICING b, XXIBM_PRODUCT_STYLE c where a.ITEM_NUMBER=b.ITEM_NUMBER and c.ITEM_NUMBER=ROUND(a.ITEM_NUMBER/1000) * 1000';
